6a5c965aa8f6251d1b315b83c04c814f0c29c052
Paul <paul.carter@cartpauj.com>
2013-02-25 14:56:33 -0700

    Fix #41 - get rid of RE's

--- views/wpf-post.php
+++ views/wpf-post.php
@@ -19,51 +19,47 @@ if ($user_ID || $this->allow_unreg())
     $out = $this->header();
     $out .= "<form action='" . WPFURL . "wpf-insert.php' name='addform' method='post' enctype='multipart/form-data'>";
     $out .= "<table class='wpf-table' width='100%'>
-			<tr>
-				<th colspan='2'>" . __("Post Reply", "mingleforum") . "</th>
-			</tr>
-			<tr>
-				<td>" . __("Subject:", "mingleforum") . "</td>
-				<td><input size='50%' type='text' name='add_post_subject' class='wpf-input' value='" . $this->get_subject($thread) . "'/></td>
-			</tr>
-			<tr>
-				<td valign='top'>" . __("Message:", "mingleforum") . "</td>
-				<td>";
-    $out .= $this->form_buttons() . $this->form_smilies();
-    $out .= "<br /><textarea " . ROW_COL . " name='message' class='wpf-textarea' >" . stripslashes($q) . "</textarea>";
-    $out .= "
-				</td>
-			</tr>";
+      <tr>
+        <th colspan='2'>" . __("Post Reply:", "mingleforum") . ' ' . $this->get_subject($thread) . "</th>
+      </tr>";
+
+    $out .= "<tr>
+            <td valign='top'>" . __("Message:", "mingleforum") . "</td>
+            <td>";
+              $out .= $this->form_buttons() . $this->form_smilies();
+              $out .= "<br /><textarea " . ROW_COL . " name='message' class='wpf-textarea' >" . stripslashes($q) . "</textarea>";
+              $out .= '<input type="hidden" name="add_post_subject" value="'.$this->get_subject($thread).'" />';
+              $out .= "
+            </td>
+          </tr>";
     $out .= apply_filters('wpwf_form_guestinfo', ''); //--weaver--
     $out .= $this->get_captcha();
 
     if ($this->options['forum_allow_image_uploads'])
     {
       $out .= "
-					<tr>
-						<td valign='top'>" . __("Images:", "mingleforum") . "</td>
-						<td colspan='2'>
-							<input type='file' name='mfimage1' id='mfimage' /><br/>
-							<input type='file' name='mfimage2' id='mfimage' /><br/>
-							<input type='file' name='mfimage3' id='mfimage' /><br/>
-						</td>
-					</tr>";
+          <tr>
+            <td valign='top'>" . __("Images:", "mingleforum") . "</td>
+            <td colspan='2'>
+              <input type='file' name='mfimage1' id='mfimage' /><br/>
+              <input type='file' name='mfimage2' id='mfimage' /><br/>
+              <input type='file' name='mfimage3' id='mfimage' /><br/>
+            </td>
+          </tr>";
     }
     $out .= "
-			<tr>
-				<td colspan='2'><input type='submit' id='wpf-post-submit' name='add_post_submit' value='" . __("Submit", "mingleforum") . "' /></td>
-				<input type='hidden' name='add_post_forumid' value='" . $this->check_parms($thread) . "'/>
-			</tr>
-			</table></form>";
+      <tr>
+        <td colspan='2'><input type='submit' id='wpf-post-submit' name='add_post_submit' value='" . __("Submit", "mingleforum") . "' /></td>
+        <input type='hidden' name='add_post_forumid' value='" . $this->check_parms($thread) . "'/>
+      </tr>
+      </table></form>";
     $this->o .= $out;
   }
 
   if (($_GET['mingleforumaction'] == "editpost"))
   {
     $this->current_view = EDITPOST;
-    $id = 0;
-    if (is_numeric($_GET['id'])) //is_numeric prevents SQL injections here
-      $id = $_GET['id'];
+    $id = (isset($_GET['id']) && !empty($_GET['id'])) ? (int)$_GET['id'] : 0;
     $thread = $this->check_parms($_GET['t']);
     $out = $this->header();
     $post = $wpdb->get_row($wpdb->prepare("SELECT * FROM {$this->t_posts} WHERE id = %d", $id));
@@ -72,28 +68,31 @@ if ($user_ID || $this->allow_unreg())
     {
       $out .= "<form action='" . WPFURL . "wpf-insert.php' name='addform' method='post'>";
       $out .= "<table class='wpf-table' width='100%'>
-				<tr>
-					<th colspan='2'>" . __("Edit Post", "mingleforum") . "</th>
-				</tr>
-				<tr>
-					<td>" . __("Subject:", "mingleforum") . "</td>
-					<td><input size='50%' type='text' name='edit_post_subject' class='wpf-input' value='" . stripslashes($post->subject) . "'/></td>
-				</tr>
-				<tr>
-					<td valign='top'>" . __("Message:", "mingleforum") . "</td>
-					<td>";
-      $out .= $this->form_buttons() . "<br/>" . $this->form_smilies();
+        <tr>
+          <th colspan='2'>" . __("Edit Post:", "mingleforum") . " " . stripslashes($post->subject) . "
+            <input type='hidden' name='edit_post_subject' value='" . stripslashes($post->text) . "' />
+          </th>
+        </tr>";
 
-      $out .= "<br /><textarea " . ROW_COL . " name='message' class='wpf-textarea' >" . stripslashes($post->text) . "</textarea>";
-      $out .= "</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td><input type='submit' id='wpf-post-submit' name='edit_post_submit' value='" . __("Save Post", "mingleforum") . "' /></td>
-					<input type='hidden' name='edit_post_id' value='" . $post->id . "'/>
-					<input type='hidden' name='thread_id' value='" . $thread . "'/>
-				</tr>
-				</table></form>";
+      if(false) //Need to enable this eventually if we're editing the first post in the thread
+        $out .= "<tr>
+              <td>" . __("Subject:", "mingleforum") . "</td>
+              <td><input size='50%' type='text' name='edit_post_subject' class='wpf-input' value='" . stripslashes($post->subject) . "'/></td>
+            </tr>";
+
+      $out .= "<tr>
+              <td valign='top'>" . __("Message:", "mingleforum") . "</td>
+              <td>";
+                $out .= $this->form_buttons() . "<br/>" . $this->form_smilies();
+                $out .= "<br /><textarea " . ROW_COL . " name='message' class='wpf-textarea' >" . stripslashes($post->text) . "</textarea>";
+                $out .= "</td>
+            </tr>
+            <tr>
+              <td colspan='2'><input type='submit' id='wpf-post-submit' name='edit_post_submit' value='" . __("Save Post", "mingleforum") . "' /></td>
+              <input type='hidden' name='edit_post_id' value='" . $post->id . "'/>
+              <input type='hidden' name='thread_id' value='" . $thread . "'/>
+            </tr>
+          </table></form>";
       $this->o .= $out;
     }
     else
